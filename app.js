b7533a1b4d93ef7c071af1af686c0ddff3f8e159
Christian E <christianelena92@gmail.com>
2021-08-31 00:39:30 -0500

    add user ShowAlert if validation is reject or incomplete

--- app.js
+++ app.js
@@ -45,6 +45,20 @@ class UI {
         }
     }
 
+// creating a div that will validate the form if its empty any input is left unfilled
+static showAlert(message, className) {
+const div = document.createElement('div'); 
+div.className = `alert alert-${className}`; 
+div.appendChild(document.createTextNode(message)); 
+const container = document.querySelector('.container');
+const form = document.querySelector('#book-form');
+container.insertBefore(div, form);  
+// Vanish in 3 seconds
+setTimeout(() => document.querySelector('.alert').remove(), 3000)
+}
+
+
+
     // clears the fields after we submit 
     static clearFields() {
         document.querySelector('#title').value = ''; 
@@ -65,14 +79,18 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const title = document.querySelector('#title').value; 
     const author = document.querySelector('#author').value; 
     const isbn = document.querySelector('#isbn').value; 
-
-// instantiate book
-const book = new Book(title, author, isbn);
-// add book to UI
-UI.addBookToList(book); 
-
-// clear fields
-UI.clearFields(); 
+//validate 
+if (title === '' || author === '' || isbn === '') {
+    UI.showAlert('Please, fill in all fields', 'danger'); 
+}else {
+    // instantiate book
+    const book = new Book(title, author, isbn);
+    // add book to UI
+    UI.addBookToList(book); 
+    
+    // clear fields
+    UI.clearFields(); 
+}
 });
 
  // Event : remove a book // event propogration
